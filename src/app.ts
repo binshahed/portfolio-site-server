f0a0758d572dddc593c2234eec5cebbbef53b40b
Shahed <151946873+shahed-emsl@users.noreply.github.com>
2024-10-22 15:00:25 +0600

    update project

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,28 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
-import { routes } from "./app/router/router";
-import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { routes } from './app/router/router';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
-
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'https://portfolio-shahed-dashboard-owdugt23u-binshaheds-projects.vercel.app',
+      'https://binshahed-portfolio.vercel.app',
+    ],
+    credentials: true,
+  }),
+);
 
 routes(app);
 
 // Global error handler
 app.use(globalErrorHandler);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 export default app;
