5a9455c27147f412a2a88ef9460c38d22dee1414
Shahed <151946873+shahed-emsl@users.noreply.github.com>
2024-10-22 16:49:27 +0600

    update corse

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:5173',
+      'http://localhost:5174',
       'https://portfolio-shahed-dashboard.vercel.app',
       'https://binshahed-portfolio.vercel.app',
     ],
